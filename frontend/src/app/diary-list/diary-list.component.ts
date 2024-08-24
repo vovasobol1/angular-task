ff2efac9e6bf68f4872e7036667ae2271322edd4
vovasobol1 <dengyrostov@gmail.com>
2024-08-24 14:16:46 +0300

    angular init

---
+++ frontend/src/app/diary-list/diary-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diary-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './diary-list.component.html',
+  styleUrl: './diary-list.component.css'
+})
+export class DiaryListComponent {
+
+}
